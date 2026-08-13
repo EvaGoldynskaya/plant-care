96d0d1df3fccacc04e27b062d59aabce872447ed
EvaGoldynskaya <eva2434578@gmail.com>
2026-08-13 16:54:34 +0300

    init client

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+      </section>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
