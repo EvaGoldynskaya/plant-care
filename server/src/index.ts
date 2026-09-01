a1484ab5ead8ce1265673c373b027b7f3ea78acf
EvaGoldynskaya <85928250+EvaGoldynskaya@users.noreply.github.com>
2026-09-02 00:38:10 +0300

    Revert "Vercel prod"

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,10 +6,6 @@ import app from "./app"
 
 const PORT = Number(process.env.PORT) || 5000
 
-if (!process.env.VERCEL) {
-	app.listen(PORT, () => {
-		console.log(`Server running on http://localhost:${PORT}`)
-	})
-}
-
-export default app
+app.listen(PORT, () => {
+	console.log(`Server running on http://localhost:${PORT}`)
+})
